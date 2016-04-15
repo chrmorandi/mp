f800fb7c04f859f2dbacdadaa4f8c56e2f66154d
Jeff Reifman <jeff@newscloud.com>
2016-04-14 21:54:42 -0700

    test live

--- frontend/controllers/MeetingController.php
+++ frontend/controllers/MeetingController.php
@@ -250,6 +250,8 @@ class MeetingController extends Controller
          //echo 'guest';
           $person = new \common\models\User;
           $identity = $person->findIdentity($actor_id);
+          var_dump($identity);
+          exit;
           if ($identity->validateAuthKey($k)) {
             Yii::$app->user->login($identity);
             // echo 'authenticated';
