5294f34d3778de15b990c609f7de3ce6c6dbe824
Jeff Reifman <jeff@newscloud.com>
2016-04-14 21:56:17 -0700

    fix find identity

--- common/models/User.php
+++ common/models/User.php
@@ -70,7 +70,8 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findIdentity($id)
     {
-        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+        return static::findOne(['id' => $id]);
+        // , 'status' => self::STATUS_ACTIVE
     }
 
     /**
