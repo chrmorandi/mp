1315fdfa26881045c72ef18300445f68b066681d
Jeff Reifman <jeff@newscloud.com>
2016-04-15 18:53:30 -0700

    fixes

--- common/components/MiscHelpers.php
+++ common/components/MiscHelpers.php
@@ -7,7 +7,7 @@ use common\models\User;
 
 class MiscHelpers  {
 
-  public static function buildCommand($meeting_id,$cmd=0,$obj_id=0,$actor_id=0,$auth_key) {
+  public static function buildCommand($meeting_id,$cmd=0,$obj_id=0,$actor_id=0,$auth_key='') {
     return Url::to(['meeting/command','id'=>$meeting_id,'cmd'=>$cmd,'actor_id'=>$actor_id,'k'=>$auth_key,'obj_id'=>$obj_id,],true);
    }
  }
