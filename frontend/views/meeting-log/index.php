cf8bf9ac7400e0c8aeb15e15dc935d902cb6a604
Jeff Reifman <jeff@newscloud.com>
2016-04-21 13:56:26 -0700

    test some content

--- frontend/views/meeting-log/index.php
+++ frontend/views/meeting-log/index.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
-//use yii\widgets\Pjax;
+use yii\widgets\Pjax;
 use frontend\models\Meeting;
 use common\models\User;
 /* @var $this yii\web\View */
@@ -15,69 +15,4 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="meeting-log-index">
 
     <h1><?php echo  Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?php // = Html::a(Yii::t('frontend', 'Create Meeting Log'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-<?php //Pjax::begin(); ?>
-<?php echo  GridView::widget([
-        'dataProvider' => $dataProvider,
-        //'filterModel' => $searchModel,
-        'columns' => [
-            //['class' => 'yii\grid\SerialColumn'],
-            [
-              'label'=>'MtgId',
-                'attribute' => 'meeting_id',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return '<div>'.$model->meeting_id.'</div>';
-                    },
-            ],
-
-            [
-              'label'=>'Subject',
-                'attribute' => 'subject',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return '<div>'.Meeting::getSubject($model->meeting_id).'</div>';
-                    },
-            ],
-            [
-              'label'=>'Actor',
-                'attribute' => 'actor_id',
-                'format' => 'raw',
-                'value' => function ($model) {
-                            return '<div>'.User::find($model->actor_id)->one()->username.'</div>';
-                    },
-            ],
-            [
-              'label'=>'Action',
-                'attribute' => 'action',
-                'format' => 'raw',
-                'value' => function ($model) {
-                            return '<div>'.$model->getMeetingLogCommand().'</div>';
-                    },
-            ],
-            [
-              'label'=>'Item',
-                'attribute' => 'item_id',
-                'format' => 'raw',
-                'value' => function ($model) {
-                            return '<div>'.$model->getMeetingLogItem().'</div>';
-                    },
-            ],
-            // 'extra_id',
-            [
-              'label'=>'Created',
-                'attribute' => 'created_at',
-                'format' => 'raw',
-                'value' => function ($model) {
-                            return '<div>'.Yii::$app->formatter->asDatetime($model->created_at,"hh:ss MMM d").'</div>';
-                    },
-            ],
-            // 'updated_at',
-            //['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-<?php //Pjax::end(); ?></div>
+<?php Pjax::end(); ?></div>
