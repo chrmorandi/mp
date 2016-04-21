def2506ce114533a73011cc3f34ed304d368eb78
Jeff Reifman <jeff@newscloud.com>
2016-04-21 14:01:31 -0700

    test wout date col

--- frontend/views/meeting-log/index.php
+++ frontend/views/meeting-log/index.php
@@ -45,7 +45,31 @@ $this->params['breadcrumbs'][] = $this->title;
                 return '<div>'.Meeting::getSubject($model->meeting_id).'</div>';
                 },
         ],
-                // 'updated_at',
+        [
+          'label'=>'Actor',
+            'attribute' => 'actor_id',
+            'format' => 'raw',
+            'value' => function ($model) {
+                    return '<div>'.User::find($model->actor_id)->one()->username.'</div>';
+                },
+        ],
+        [
+          'label'=>'Action',
+            'attribute' => 'action',
+            'format' => 'raw',
+            'value' => function ($model) {
+                  return '<div>'.$model->getMeetingLogCommand().'</div>';
+                },
+        ],
+        [
+          'label'=>'Item',
+            'attribute' => 'item_id',
+            'format' => 'raw',
+            'value' => function ($model) {
+                        return '<div>'.$model->getMeetingLogItem().'</div>';
+                },
+        ],
+        // 'updated_at',
         // 'extra_id',
         //['class' => 'yii\grid\ActionColumn'],
     ],
