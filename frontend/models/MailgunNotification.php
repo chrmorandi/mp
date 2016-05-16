02d40d929fdd1fd642dd3a83f93f0ab7e109dcf7
Jeff Reifman <jeff@newscloud.com>
2016-05-16 14:48:55 -0700

    tet

--- frontend/models/MailgunNotification.php
+++ frontend/models/MailgunNotification.php
@@ -89,6 +89,8 @@ class MailgunNotification extends \yii\db\ActiveRecord
         //echo $m->id.'<br />';
         $raw_response = $yg->get($m->url);
         $response = $raw_response->http_response_body;
+        $stripped_text = $response['stripped-text'];
+        echo 'st1:'.$stripped_text;
         var_dump ($response);
 
         exit;
