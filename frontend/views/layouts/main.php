0698f1597a3cfac59996bfe940c38c6b3d54874e
Jeff Reifman <jeff@newscloud.com>
2016-09-11 17:26:03 -0700

    Add privacy and tos

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -125,13 +125,16 @@ AppAsset::register($this);
         <div class="container">
           <p class="pull-left">
             <?php
-              echo Html::a('&copy; Lookahead '.date('Y'),'http://lookahead.io',['class'=>'itemHide']).'';
+              echo Html::a('&copy; Lookahead '.date('Y'),'http://lookahead.io',['class'=>'itemHide']);
+              echo Html::tag('span',' . ',['class'=>'itemHide']);
+              echo Html::a(Yii::t('frontend','privacy'),Url::to(['/site/privacy']));
+              echo Html::tag('span',' . '.Html::a(Yii::t('frontend','terms'),Url::to(['/site/tos'])));
             ?>
         <p class="pull-right">
         <?= Html::a('@meetingio','https://twitter.com/intent/user?screen_name=meetingio') ?>
+        <?= Html::tag('span',' . '.Html::a(Yii::t('frontend','blog'),Url::to('https://blog.meetingplanner.io'))); ?>
         <?php
         if (!Yii::$app->user->isGuest) {
-          echo Html::tag('span',' . '.Html::a(Yii::t('frontend','blog'),Url::to('https://blog.meetingplanner.io')),['class'=>'itemHide']);
           echo Html::tag('span',' . '.Html::a(Yii::t('frontend','features'),Url::to(['/features'])),['class'=>'itemHide']);
           echo Html::tag('span',' . '.Html::a(Yii::t('frontend','about'),Url::to(['/about'])),['class'=>'itemHide']);
         }
--- frontend/views/site/tos.php
+++ frontend/views/site/tos.php
@@ -2,12 +2,100 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'Terms of Service - Meeting Planner';
+$this->title = 'Terms of Conditions and Use';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-privacy">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Meeting Planner is a new app designed to make scheduling as simple as it should be. The Terms of Service still needs to be written.</p>
+    <p>Meeting Planner is a web service designed to make scheduling as simple as it should be.</p>
 
+    <h3>
+    	1. Terms
+    </h3>
+
+    <p>
+    	By accessing this web site, you are agreeing to be bound by these
+    	web site Terms and Conditions of Use, all applicable laws and regulations,
+    	and agree that you are responsible for compliance with any applicable local
+    	laws. If you do not agree with any of these terms, you are prohibited from
+    	using or accessing this site. The materials contained in this web site are
+    	protected by applicable copyright and trade mark law.
+    </p>
+
+    <h3>
+    	2. Use License
+    </h3>
+
+    <ol type="a">
+    	<li>
+    		Permission is granted to temporarily download one copy of the materials
+    		(information or software) on Meeting Planner's web site for personal,
+    		non-commercial transitory viewing only. This is the grant of a license,
+    		not a transfer of title, and under this license you may not:
+
+    		<ol type="i">
+    			<li>modify or copy the materials;</li>
+    			<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+    			<li>attempt to decompile or reverse engineer any software contained on Meeting Planner's web site;</li>
+    			<li>remove any copyright or other proprietary notations from the materials; or</li>
+    			<li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+    		</ol>
+    	</li>
+    	<li>
+    		This license shall automatically terminate if you violate any of these restrictions and may be terminated by Meeting Planner at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
+    	</li>
+    </ol>
+
+    <h3>
+    	3. Disclaimer
+    </h3>
+
+    <ol type="a">
+    	<li>
+    		The materials on Meeting Planner's web site are provided "as is". Meeting Planner makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Meeting Planner does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.
+    	</li>
+    </ol>
+
+    <h3>
+    	4. Limitations
+    </h3>
+
+    <p>
+    	In no event shall Meeting Planner or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on Meeting Planner's Internet site, even if Meeting Planner or a Meeting Planner authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+    </p>
+
+    <h3>
+    	5. Revisions and Errata
+    </h3>
+
+    <p>
+    	The materials appearing on Meeting Planner's web site could include technical, typographical, or photographic errors. Meeting Planner does not warrant that any of the materials on its web site are accurate, complete, or current. Meeting Planner may make changes to the materials contained on its web site at any time without notice. Meeting Planner does not, however, make any commitment to update the materials.
+    </p>
+
+    <h3>
+    	6. Links
+    </h3>
+
+    <p>
+    	Meeting Planner has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Meeting Planner of the site. Use of any such linked web site is at the user's own risk.
+    </p>
+
+    <h3>
+    	7. Site Terms of Use Modifications
+    </h3>
+
+    <p>
+    	Meeting Planner may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.
+    </p>
+
+    <h3>
+    	8. Governing Law
+    </h3>
+
+    <p>
+    	Meeting Planner is owned and operated by NewsCloud Consulting. Any claim relating to Meeting Planner's web site shall be governed by the laws of the State of Washington without regard to its conflict of law provisions.
+    </p>
+
+    <p>Please read our <em><?= Html::a('Privacy Policy',['/site/privacy']);?></em></p>
 </div>
