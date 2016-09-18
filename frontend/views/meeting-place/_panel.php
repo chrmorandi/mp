30bcbf33d74e8fb595f434c3edb129fa7a8969ef
Jeff Reifman <jeff@newscloud.com>
2016-09-18 12:07:25 -0700

    fix panel

--- frontend/views/meeting-place/_panel.php
+++ frontend/views/meeting-place/_panel.php
@@ -5,14 +5,15 @@ use \kartik\switchinput\SwitchInput;
 use \common\components\MiscHelpers;
 ?>
 <div id="notifierPlace" class="alert-info alert fade in" style="display:none;">
-<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-<?php echo Yii::t('frontend',"We'll automatically notify the organizer when you're done making changes."); ?>
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  <?php echo Yii::t('frontend',"We'll automatically notify the organizer when you're done making changes."); ?>
 </div>
 <div class="panel panel-default">
   <!-- Default panel contents -->
   <div class="panel-heading" role="tab" id="headingWhere">
     <div class="row">
-      <div class="col-lg-10 col-md-10 col-xs-10" ><h4 class="meeting-place"><a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseWhere" aria-expanded="true" aria-controls="collapseWhere"><?= Yii::t('frontend','Where') ?></a></h4><p>
+      <div class="col-lg-10 col-md-10 col-xs-10" >
+        <h4 class="meeting-place"><a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseWhere" aria-expanded="true" aria-controls="collapseWhere"><?= Yii::t('frontend','Where') ?></a></h4><p>
         <div class="hint-text heading-pad">
         <?php if ($placeProvider->count<=1) { ?>
           <?= Yii::t('frontend','add places for participants or switch to \'virtual\'') ?>
@@ -24,14 +25,16 @@ use \common\components\MiscHelpers;
         <?php if ($placeProvider->count>1 && ($model->isOrganizer() || $model->meetingSettings['participant_choose_place'])) { ?>
           <?= Yii::t('frontend','you can also choose the place') ?>
         <?php }?>
-      </div></div>
+      </div>
+    </div>
 
 <?php
   if (!$model->isOrganizer()) {
     // To Do: Check Meeting Settings whether participant can add places
   }
 ?>
-      <div class="col-lg-2 col-md-2 col-xs-2" ><div style="float:right;">
+      <div class="col-lg-2 col-md-2 col-xs-2" >
+        <div style="float:right;">
         <?php
           if ($model->isOrganizer() || $model->meetingSettings->participant_add_place) {
           ?>
@@ -58,10 +61,10 @@ use \common\components\MiscHelpers;
           <?php
           }
         ?>
-              </div>
+        </div>
     </div>
-  </div>
-  </div>
+  </div> <!-- end row -->
+</div> <!-- end panel heading -->
   <div id="collapseWhere" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingWhere">
     <div class="panel-where">
       <div id="placeMessage" class="alert-info alert fade in hidden">
@@ -96,5 +99,6 @@ use \common\components\MiscHelpers;
 
     </table>
       </div>
-</div>
-</div>
+    </div> <!-- end class panel-where -->
+  </div> <!-- end collapse panel where -->
+</div> <!-- end panel -->
