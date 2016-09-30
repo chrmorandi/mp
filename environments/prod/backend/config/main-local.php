27c0a40f7355fad44f0bb874c6f0ad30121027d2
Deploy User <deploy@meetingplanner.io>
2016-09-29 17:51:40 -0700

    update gitignore

--- environments/prod/backend/config/main-local.php
+++ environments/prod/backend/config/main-local.php
@@ -3,7 +3,8 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'mp09LZPtq2MeByMdn2e1UX06m804zxy2',
+		'enableCookieValidation' => true,
         ],
     ],
 ];
