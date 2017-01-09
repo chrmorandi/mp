b06c2022300c364a89953c9329810c6e0cf0fabb
Alexander Makarov <sam@rmcreative.ru>
2017-01-10 01:47:30 +0300

    Removed unused method override

--- api/modules/v1/Module.php
+++ api/modules/v1/Module.php
@@ -11,14 +11,4 @@ class Module extends \yii\base\Module
      * @inheritdoc
      */
     public $controllerNamespace = 'api\modules\v1\usertoken\controllers';
-
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        parent::init();
-
-        // custom initialization code goes here
-    }
 }
