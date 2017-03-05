8eda5b0cab5ca29f69538bad490af65c28c898ed
Jeff Reifman <jeff@newscloud.com>
2017-03-05 10:41:52 -0800

    remove ably

--- frontend/assets/MeetingAsset.php
+++ frontend/assets/MeetingAsset.php
@@ -8,6 +8,7 @@ class MeetingAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
       'css/bootstrap-combobox.css',
+      //'css/shepherd-theme-arrows.css',
     ];
     public $js = [
       'js/meeting.js',
@@ -15,6 +16,11 @@ class MeetingAsset extends AssetBundle
       'js/jstz.min.js',
       'js/bootstrap-combobox.js',
       'js/create_place.js',
+      /*
+      'js/tether.min.js',
+      'js/shepherd.min.js',
+      'js/meeting_tour.js',
+      */
     ];
     public $depends = [
         'yii\web\YiiAsset',
