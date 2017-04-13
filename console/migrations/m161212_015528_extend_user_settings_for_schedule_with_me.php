5fe15ad9b55162987be68cfffee1c469e731d84f
Jeff Reifman <jeff@newscloud.com>
2017-04-13 16:23:35 -0700

    fix again

--- console/migrations/m161212_015528_extend_user_settings_for_schedule_with_me.php
+++ console/migrations/m161212_015528_extend_user_settings_for_schedule_with_me.php
@@ -16,12 +16,12 @@ class m161212_015528_extend_user_settings_for_schedule_with_me extends Migration
     for past installations, this code turned on the setting for prior users
     but it doesn't work after the guide property has been added to user_setting
     needs to be rewritten */
+    /*
     $all = UserSetting::find()->all();
     foreach ($all as $us) {
       $us->schedule_with_me = UserSetting::SETTING_ON;
       $us->update();
-    }
-    
+    }*/
   }
 
   public function down()
