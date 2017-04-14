4ad43ccde1371b3448206dc54106e69677158b8a
Jeff Reifman <jeff@newscloud.com>
2017-04-13 19:27:17 -0700

    patch

--- frontend/web/js/flags.js
+++ frontend/web/js/flags.js
@@ -1,4 +1,7 @@
-$(document).ready().on("tap",$('#flagTarget'),function(e){
+$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+
+$('#flagTarget').on("tap",function(e){
+  alert('i');
   language = $( this ).attr('id');
   $.ajax({
      url: $('#url_prefix').val()+'/user-setting/setlanguage',
