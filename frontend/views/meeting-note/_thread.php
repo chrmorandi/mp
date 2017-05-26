732e99c687ced0a68399ef582b4aded2374b9230
Jeff Reifman <jeff@newscloud.com>
2017-05-25 17:09:13 -0700

    Remove no messages yet

--- frontend/views/meeting-note/_thread.php
+++ frontend/views/meeting-note/_thread.php
@@ -14,7 +14,6 @@ if ($noteProvider->count>0):
 </table>
 <?php else: ?>
 <div class="panel-body">
-<p><?= Yii::t('frontend','No messages yet.'); ?></p>
   <div class="text-center">
     <?= Yii::t('frontend','Click');?> <?= Html::a('<span class="glyphicon glyphicon-plus" aria-hidden="true"></span><span class="glyphicon glyphicon-comment button-pad-left" aria-hidden="true"></span>', 'javascript:void(0);', ['class' => 'btn btn-primary mini-button mini-button-pad','title'=>'Add participants','onclick'=>'showNote();']); ?> <?= Yii::t('frontend','to send messages to others'); ?>
   </div>
